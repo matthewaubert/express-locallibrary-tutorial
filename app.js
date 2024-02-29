5a1123bff07f911b0ec42247d88be291daa6f444
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-02-28 20:31:17 -0500

    Prep app before publishing

--- app.js
+++ app.js
@@ -7,14 +7,37 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog'); // import routes for 'catalog' area of site
+const compression = require('compression');
+const helmet = require('helmet');
 
-const app = express();
+const app = express(); // create the Express application object
+
+// set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// apply rate limiter to all requests
+app.use(limiter);
+
+// add helmet to the middleware chain
+// set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      'script-src': ["'self'", 'code.query.com', 'cdn.jsdelivr.net'],
+    },
+  })
+);
 
 // set up mongoose connection
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-const mongoDB =
+
+const dev_db_url =
   'mongodb+srv://matthewaubert:VEJ7I6WPSkBarfJI@cluster0.ediprkq.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -29,6 +52,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); // compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
